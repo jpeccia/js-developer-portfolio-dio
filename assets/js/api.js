b2f63c151af7634119a2d701ad9f87e6f3f0f4fb
Joao Otavio Peccia <joaootaviopeccia0@gmail.com>
2023-03-04 00:19:22 -0300

    Atualizar nome de exemplo

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/jpeccia/js-developer-portfolio-dio/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,29 @@
-(async () => {
-    
-    const profileData = await fetchProfileData()
-    console.log(profileData)
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    const name = document.getElementById('profile.name')
+    const job = document.getElementById('profile.job')
+    const location = document.getElementById('profile.location')
+    const phone = document.getElementById('profile.phone')
+    const email = document.getElementById('profile.email')
 
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    name.innerText = profileData.name
+
+    job.innerText = profileData.job
+
+    location.innerText = profileData.location
+
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+
+    email.innerText = profileData.email
+    email.href = `mail:${profileData.email}`
+
+}
+
+(async () => {
+    const profileData = await fetchProfileData()
+    updateProfileInfo(profileData)
 })()
